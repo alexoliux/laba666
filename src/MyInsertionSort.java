ac49e7ce7799f5120c7f0909bd676c6c5a62094c
Dinh Cuong <dinhcuong.firewin99@gmail.com>
2020-10-05 17:36:08 +0300

    Initial commit

---
+++ src/MyInsertionSort.java
@@ -0,0 +1,33 @@
+package ru.mirea.pr6;
+
+
+
+public class MyInsertionSort
+
+{
+
+    public static void insertionSort (Comparable[] list)
+
+    {
+
+        Comparable temp;
+
+        int j;
+
+        for (int i = 1; i < list.length; ++i)
+
+        {
+
+            temp = list[i];
+
+            for (j = i; j > 0 && list[j - 1].compareTo(temp) > 0; --j)
+
+                list[j] = list[j - 1];
+
+            list[j] = temp;
+
+        }
+
+    }
+
+}
---
+++ src/SortingStudentsByGPA.java
@@ -0,0 +1,23 @@
+package ru.mirea.pr6;
+
+
+
+import java.util.Comparator;
+
+
+
+public class SortingStudentsByGPA implements Comparator<Student>
+
+{
+
+    @Override
+
+    public int compare(Student o1, Student o2)
+
+    {
+
+        return Float.compare(o1.getGpa(), o2.getGpa());
+
+    }
+
+}
